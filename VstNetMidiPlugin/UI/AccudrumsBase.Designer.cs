6827d97eb0e53087d161f0cad856d81bef54c550
wilbe <wilbe@LAPTOP-MU6C21RG>
2016-06-23 11:42:36 +0200

    Working DLL x64 Build and midi in and output

---
+++ VstNetMidiPlugin/UI/AccudrumsBase.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace VstNetMidiPlugin.UI {
+    partial class AccudrumsBase {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pluginEditorView = new VstNetMidiPlugin.UI.PluginEditorView();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(106, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hier komt Accudrums";
+            // 
+            // pluginEditorView
+            // 
+            this.pluginEditorView.Location = new System.Drawing.Point(111, 136);
+            this.pluginEditorView.Name = "pluginEditorView";
+            this.pluginEditorView.Size = new System.Drawing.Size(215, 99);
+            this.pluginEditorView.TabIndex = 1;
+            // 
+            // AccudrumsBase
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.Controls.Add(this.pluginEditorView);
+            this.Controls.Add(this.label1);
+            this.Name = "AccudrumsBase";
+            this.Size = new System.Drawing.Size(463, 379);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private PluginEditorView pluginEditorView;
+    }
+}
---
+++ VstNetMidiPlugin/UI/AccudrumsBase.cs
@@ -0,0 +1,20 @@
+﻿using Jacobi.Vst.Framework;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace VstNetMidiPlugin.UI {
+    public partial class AccudrumsBase : UserControl {
+        public AccudrumsBase() {
+            InitializeComponent();
+        }
+
+        internal bool InitializeParameters(List<VstParameterManager> parameters) {
+            pluginEditorView.InitializeParameters(parameters);
+            return true;
+        }
+
+        internal void ProcessIdle() {
+            // TODO: short idle processing here
+        }
+    }
+}
